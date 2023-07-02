08d16621b706d9270b35fa9132e231c7241e8256
baejunil <bjo6300@naver.com>
2023-07-03 01:28:42 +0900

    feat : BaseModel 적용

--- question_api/answer/models.py
+++ question_api/answer/models.py
@@ -4,13 +4,14 @@ from django.db import models
 from django.db import models
 
 from question.models import Question
+from question_api.basemodel import BaseModel
 
 # Create your models here.
-class Answer(models.Model):
+class Answer(BaseModel):
     question_id = models.ForeignKey(Question, on_delete=models.CASCADE) # onetomany 설정, 질문 삭제 시 답변도 삭제
     content = models.CharField(max_length=100) # 내용
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    # created_at = models.DateTimeField(auto_now_add=True)
+    # updated_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return self.title
--- question_api/question/models.py
+++ question_api/question/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-
+from question_api.basemodel import BaseModel
 # Create your models here.
-class Question(models.Model):
+class Question(BaseModel):
     title = models.CharField(max_length=30) # 제목
     content = models.CharField(max_length=100) # 내용
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    # created_at = models.DateTimeField(auto_now_add=True)
+    # updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
